0f2b51a06c9f8ec96c8027abc753de8845af1946
Khafiatov-Bogdan <karlapingus20@gmail.com>
2026-09-03 13:35:35 +0400

    feat: create class User, Transaction, Block AND comments

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,11 @@ import java.util.ArrayList;
 class User {
     private int Index = 0;
     private float Balanse = 0;
+    /*
+    * Функция осуществления перевода, создание генезис блока
+    * Проверка с балансом, проверка хеша
+    * */
+
 }
 
 class Transaction{
@@ -10,6 +15,7 @@ class Transaction{
     private int Index_sender;
     private int Index_giver;
     private float belence;
+
 }
 
 class Block{
@@ -17,6 +23,15 @@ class Block{
     public ArrayList<Transaction> trans_list;
     String hash_past;
     String Hash_now;
+
+
+
+/*
+* @Override
+    public int hashCode() {
+        return Objects.hash(Index, trans_list);
+    }
+*/
 
     // hashCode() - ункция для получения хеша
     //hash_past
